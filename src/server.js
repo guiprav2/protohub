736b972d81dace339bd36db9429d7da171bf3141
Gui Prá <gui@supersaas.com>
2021-12-06 16:46:42 +0100

    WIP: CRDT endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
+import * as Crdt from 'crdt';
 import cors from 'cors';
 import crypto from 'crypto';
 import errorMiddleware from './errorMiddleware.js';
 import express from 'express';
+import ews from 'express-ws';
+import wss from 'websocket-stream/stream.js';
 import feathersService from './feathersService.js';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
@@ -12,6 +15,7 @@ let port = process.env.PORT || 3000;
 let trustProxy = JSON.parse(process.env.TRUST_PROXY || '0');
 let rateLimitWindow = JSON.parse(process.env.RATE_LIMIT_WINDOW || '60000');
 let rateLimitMaxReqs = JSON.parse(process.env.RATE_LIMIT_MAX_REQS || '50');
+let crdt = {};
 let scrubPasswords = x => scrub(x, ['password'], 'test');
 
 if (trustProxy) {
@@ -26,6 +30,7 @@ app.use(rateLimit({
 app.use(helmet());
 app.use(cors());
 
+ews(app, null, { perMessageDeflate: false });
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -35,9 +40,12 @@ app.use((req, res, next) => {
   next();
 });
 
+let reqPath = req =>
+  decodeURIComponent(new URL(`http://unused/${req.originalUrl}`).pathname)
+  .slice(1);
+
 app.use((req, res, next) => {
-  let url = new URL(`http://unused/${req.originalUrl}`);
-  let path = decodeURIComponent(url.pathname).slice(1);
+  let path = reqPath(req);
 
   console.log(req.method, path, JSON.stringify({
     ip: req.ip,
@@ -53,6 +61,26 @@ app.use((req, res, next) => {
 
 app.get('/status', (req, res) => res.send('OK'));
 app.post('/:ns/log', (req, res) => res.sendStatus(204));
+
+app.ws('/crdt/:ns/:id', (ws, req) => {
+  let { ns, id } = req.params, docs = crdt[ns] = crdt[ns] || {};
+  let doc = docs[id] = docs[id];
+
+  if (!doc) {
+    doc = docs[id] = { doc: new Crdt.Doc(), peerDocs: new Set() };
+    doc.doc.on('row_update', x => console.log('UPDATE', reqPath(req), JSON.stringify(x)));
+  }
+
+
+  let peerDoc = new Crdt.Doc();
+  doc.peerDocs.add(peerDoc);
+
+  let docStream = doc.doc.createStream();
+  docStream.pipe(peerDoc.createStream()).pipe(docStream);
+
+  let stream = wss(ws, { binary: true });
+  stream.pipe(peerDoc.createStream()).pipe(stream);
+});
 
 app.post('/:ns/:collection', async (req, res) => {
   try {
